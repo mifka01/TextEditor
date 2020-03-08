a76c1c3b14455b31bf9fb2ca0f75f4455b6fb738
Marvin Chong <marvincht@gmail.com>
2020-03-08 16:50:08 +0800

    Added keyboard shortcuts to MAC

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog
 import os
 from random import choice
+from platform import system as platform
 
 
 # Colors
@@ -393,12 +394,25 @@ text_editor.master.geometry("900x700")
 text_editor.master.iconbitmap('icons/Notepad.ico')
 
 # Binds
-text_editor.master.bind('<Control-s>', text_editor.ctrlS)
-text_editor.master.bind('<Control-o>', text_editor.ctrlO)
-text_editor.master.bind('<Control-q>', text_editor.ctrlQ)
-text_editor.master.bind('<Control-n>', text_editor.ctrlN)
-text_editor.master.bind('<Control-Left>', text_editor.left_file)
-text_editor.master.bind('<Control-Right>', text_editor.right_file)
-text_editor.master.protocol("WM_DELETE_WINDOW", lambda: text_editor.ctrlQ(""))
+if platform() == 'Darwin':
+    text_editor.master.bind('<Command-s>', text_editor.ctrlS)
+    text_editor.master.bind('<Command-o>', text_editor.ctrlO)
+    text_editor.master.bind('<Command-q>', text_editor.ctrlQ)
+    text_editor.master.bind('<Command-n>', text_editor.ctrlN)
+    text_editor.master.bind('<Command-Left>', text_editor.left_file)
+    text_editor.master.bind('<Command-Right>', text_editor.right_file)
+    text_editor.master.protocol(
+        "WM_DELETE_WINDOW", lambda: text_editor.ctrlQ("")
+    )
+else:
+    text_editor.master.bind('<Control-s>', text_editor.ctrlS)
+    text_editor.master.bind('<Control-o>', text_editor.ctrlO)
+    text_editor.master.bind('<Control-q>', text_editor.ctrlQ)
+    text_editor.master.bind('<Control-n>', text_editor.ctrlN)
+    text_editor.master.bind('<Control-Left>', text_editor.left_file)
+    text_editor.master.bind('<Control-Right>', text_editor.right_file)
+    text_editor.master.protocol(
+        "WM_DELETE_WINDOW", lambda: text_editor.ctrlQ("")
+    )
 
 root.mainloop()  # Start the program
